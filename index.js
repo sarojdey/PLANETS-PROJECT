6f1e7d44d99f506e2535f81b3239c8da5f961970
Saroj Kumar Dey <sarojkumardey.contact@gmail.com>
2023-08-29 00:28:51 +0530

    completeing the project

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const { parse } = require("csv-parse");
 const fs = require("fs");
 
-const results = [];
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
 
 fs.createReadStream("kepler_data.csv")
   .pipe(
@@ -11,11 +20,17 @@ fs.createReadStream("kepler_data.csv")
     })
   )
   .on("data", (data) => {
-    results.push(data);
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
   })
   .on("end", () => {
-    console.log(results);
-    console.log("done");
+    console.log(`no. of habitable planet is ${habitablePlanets.length}`);
+    console.log(
+      habitablePlanets.map((planet) => {
+        return planet["kepler_name"];
+      })
+    );
   })
   .on("error", (err) => {
     console.log(err);
