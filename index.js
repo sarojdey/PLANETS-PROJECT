b7b55bf34ed681c12aea91abc0d0ce86cee68fbe
Saroj Kumar Dey <sarojkumardey.contact@gmail.com>
2023-08-30 00:09:33 +0530

    comments

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { parse } = require("csv-parse");
+const { parse } = require("csv-parse"); // using object destructuring to import only parse() from csv-parse
 const fs = require("fs");
 
 const habitablePlanets = [];
@@ -6,16 +6,16 @@ const habitablePlanets = [];
 function isHabitablePlanet(planet) {
   return (
     planet["koi_disposition"] === "CONFIRMED" &&
-    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] > 0.36 &&  // returns boolean if a planet is habitable or not
     planet["koi_insol"] < 1.11 &&
     planet["koi_prad"] < 1.6
   );
 }
 
-fs.createReadStream("kepler_data.csv")
-  .pipe(
+fs.createReadStream("kepler_data.csv")  // fs.createReadStream to create a readable stream
+  .pipe( // we directly pipe it into parse() because createReadStream() creates a buffer data for each line of csv file on by one
     parse({
-      comment: "#",
+      comment: "#",  // parsing the readable stream 
       columns: true,
     })
   )
@@ -28,7 +28,7 @@ fs.createReadStream("kepler_data.csv")
     console.log(`no. of habitable planet is ${habitablePlanets.length}`);
     console.log(
       habitablePlanets.map((planet) => {
-        return planet["kepler_name"];
+        return planet["kepler_name"]; // maping the result objects based only on their name
       })
     );
   })
